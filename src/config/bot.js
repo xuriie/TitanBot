fe9183ac48f0b1683a1c254c1badd93457cb99f2
xuriie <cryii4vr@gmail.com>
2026-09-04 11:30:37 -0500

    Update bot status to 'I love flan 😋'

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "",     // this is what people actually see
+        state: "I love flan 😋",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
